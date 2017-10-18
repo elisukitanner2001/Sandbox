46d9a85b2f2dd2bb621f6b60692091bc5a17330a
compsci <compsci@compsci>
2017-10-18 10:29:20 -0600

    worked out yes no

--- src/Runner.java
+++ src/Runner.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Runner {
 
 	public static void main(String[] args) {
@@ -5,7 +7,7 @@ public class Runner {
 		
 		/*** Testing integer division -- truncates! Does NOT round off! ***/
 		/*** % modulus returns the remainder of a division ***/
-		int numberBits = 10;
+	/*	int numberBits = 10;
 		int bite = 8; // spelled wrong to avoid a keyword conflict byte
 		int nibble = 4;
 		int byteAnswer = 0, byteRemainder = 0;
@@ -16,6 +18,21 @@ public class Runner {
 
 		System.out.println("bits = " + numberBits + " bytes = " 
 		+ byteAnswer + " remainder = " + byteRemainder);
-
-	}
+*/
+		
+		Scanner kbd = new Scanner(System.in);
+		System.out.println("Hello enter your answer (Y/N)");
+		String answer = kbd.nextLine().toUpperCase();
+		System.out.println("You have entered: "+ answer);
+		//decision time
+		if(answer.equals("Y")|| answer.equals("YES")) {
+			System.out.println("You have selected YES!");
+		
+		}else if(answer.equals("N")|| answer.equals("NO")) {
+			System.out.println("You have selected No!");
+			
+		}else {
+			System.out.println("UMMMM try again please. Following directions should help to");
+		}
+}
 }
